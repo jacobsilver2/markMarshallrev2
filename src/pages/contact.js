7e36883ce357452cce5909230dfd219d15a7c02e
Jacob <jacobsilver2@mac.com>
2021-06-26 14:55:01 -0400

    updated contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,10 +14,15 @@ const ContactPage = () => {
           justifyContent: "center",
           alignContent: "center",
           textAlign: "center",
+          fontSize: "clamp(3vw, 3.5vw, 4vw)",
         }}
       >
-        For more info about licensing or an original score for film, tv, or
-        video games, please email me at futurerelicsmusic at gmail.com
+        <p style={{ paddingBottom: "1em" }}>
+          For more info about licensing or an original score for film, tv, or
+          video games, please contact me at:
+        </p>
+        <p>futurerelicsmusic at gmail.com</p>
+        <p>646-715-6538</p>
       </div>
     </>
   )
