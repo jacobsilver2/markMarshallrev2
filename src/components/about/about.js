f7c1d4263fbc2544dbaa0ee928ceb26040d0b56f
Jacob <jacobsilver2@mac.com>
2021-06-26 10:34:09 -0400

    added image and contact info

---
+++ src/components/about/about.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Img from "gatsby-image"
+import styles from "./aboutStyle.module.css"
+const About = ({ img }) => {
+  return (
+    <div className={styles.container}>
+      <div style={{ color: "white" }}>
+        <p style={{ padding: "1em 0" }}>
+          Mark Marshall is a composer/multi-instrumentalist who enjoys working
+          with a variety of tonal mediums. Equally comfortable with organic and
+          sounds as well synthetic sounds, Mark likes to jump through musical
+          time from past to present and future.
+        </p>
+        <p style={{ padding: "1em 0" }}>
+          Mark has been in the music industry for many years, working with
+          notable artists such as Phil Lesh, Levon Helm, Sara Bareilles, Lee Ann
+          Womack, Elizabeth, and the Catapult, Abby Ahmad, Fife & Drom, Jenna
+          Nicholls, Amy Helm, Sister Sparrow, and many more in the studio or on
+          tour as a musician, producer, composer or musical director. Mark has
+          played many major music festivals such as New Orleans Jazz Fest,
+          Mountain Jam, and others. He has toured internationally and appeared a
+          number of times on the BBC as well as American TV. Marshall has also
+          worked alongside some of the most notable musicians in the industry.
+        </p>
+        <p style={{ padding: "1em 0" }}>
+          Mark has also contributed to Premier Guitar magazine as a writer,
+          published his own book on music theory, and released a video series on
+          recording and mixing.
+        </p>
+        <p style={{ padding: "1em 0" }}>
+          Mark is a prolific writer and has composed music for TV Shows, films,
+          video games, and tv commercials. Marshall has extensive experience in
+          composing for full symphonic orchestras, to rock and pop outfits to
+          creating experimental electronic Avante-Garde music.
+        </p>
+        <p style={{ padding: "1em 0" }}>
+          Mark doesn't hear borders in music. Just the emotion that each style
+          evokes.
+        </p>
+      </div>
+      <Img fadeIn fluid={img} />
+    </div>
+  )
+}
+
+export default About
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,20 @@ const ContactPage = () => {
   return (
     <>
       <SEO title="contact" />
-      Hello from the contact page
+      <div
+        style={{
+          color: "white",
+          display: "grid",
+          height: "100%",
+          padding: "0 2em",
+          justifyContent: "center",
+          alignContent: "center",
+          textAlign: "center",
+        }}
+      >
+        For more info about licensing or an original score for film, tv, or
+        video games, please email me at futurerelicsmusic at gmail.com
+      </div>
     </>
   )
 }
